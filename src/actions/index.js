070ce6bbbae92bb56f16deac6b6ab3a7df522f03
Jayalakshmi S <jayalakshmis692@gmail.com>
2021-04-21 13:29:25 +0530

    CORRECT_GUESS action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,14 @@
-import axios from  'axios'
+import axios from  'axios';
+
+export const actionType = {
+    CORRECT_GUESS : 'CORRECT_GUESS'
+}
+
+export function correctGuess () {
+    return {type : actionType.CORRECT_GUESS};
+}
 
 export const getSecretWord = () =>{
     return axios.get('http://localhost:3000')
-    .then(response=>response.data)
-}
+    .then(response => response.data)
+} 
